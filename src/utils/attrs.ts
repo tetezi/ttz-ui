cfe7b2c6d006628998d4474335d0bdec533420cf
wxw <1913483156@qq.com>
2024-04-15 12:01:45 +0800

    <feat>添加useLocalModel工具方法

--- src/utils/attrs.ts
+++ src/utils/attrs.ts
@@ -1,5 +1,23 @@
-import { omit, upperFirst } from "lodash";
-import { ref, computed, unref } from "vue";
+import {
+  cloneDeep,
+  get,
+  isObject,
+  omit,
+  set,
+  upperFirst,
+  type PropertyPath,
+} from "lodash";
+import {
+  ref,
+  computed,
+  unref,
+  getCurrentInstance,
+  watchSyncEffect,
+  toRaw,
+  type Ref,
+  type ModelRef,
+  type UnwrapRef,
+} from "vue";
 
 /**获取当前组件事件，用于传递子组件做事件继承 */
 export function getInheritanceEvent<T extends string>(
@@ -15,6 +33,9 @@ export function getInheritanceEvent<T extends string>(
   return result;
 }
 
+/**
+ * 本地模式props，与组件props合并
+ */
 export function useLocalProps<
   Props extends Recordable,
   ShortEvent extends Recordable
@@ -57,4 +78,35 @@ export function useLocalProps<
     eventHandler?.(...args);
   };
   return { getProps, setProps, emitEvent };
+}
+/**
+ * 本地模式modelValue，如有使用v-model则使用原modelValue，否则生成一个本地modelValue
+ * PS:vue的defineModel语法糖内部也会实现本地模式，但返回的是简单类型的customRefRef，无法监听深层修改，故作修改
+ */
+export function useLocalModel<T>(modelValue: ModelRef<T>) {
+  const rawProps = getCurrentInstance()!.vnode!.props;
+  const localModelValue: ModelRef<T> | Ref<T> =
+    rawProps && "modelValue" in rawProps
+      ? modelValue
+      : (ref(cloneDeep(toRaw(unref(modelValue)))) as Ref<T>);
+  function getFieldsValue(key: PropertyPath) {
+    return get(unref(localModelValue), key);
+  }
+  function setFieldsValue(key: PropertyPath, val: any) {
+    const model = unref(localModelValue);
+    if (isObject(model)) {
+      set(model, key, val);
+    }
+  }
+  function setValue(val: T) {
+    localModelValue.value = val;
+  }
+  const getValue = computed(() => unref(localModelValue));
+  return {
+    localModelValue,
+    setValue,
+    getValue,
+    getFieldsValue,
+    setFieldsValue,
+  };
 }
